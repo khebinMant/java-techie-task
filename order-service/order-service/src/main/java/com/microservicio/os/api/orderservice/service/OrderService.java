06210cd647541cc01bcd3a247c233f4372f887a7
khabLexander <mantillagka@hotmail.com>
2023-01-04 23:44:23 -0500

    cb

--- order-service/order-service/src/main/java/com/microservicio/os/api/orderservice/service/OrderService.java
+++ order-service/order-service/src/main/java/com/microservicio/os/api/orderservice/service/OrderService.java
@@ -1,12 +1,10 @@
 package com.microservicio.os.api.orderservice.service;
 
-import org.h2.mvstore.tx.Transaction;
 import org.slf4j.Logger;
 
 
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
@@ -27,21 +25,23 @@ public class OrderService {
     private OrderRepository repository;
 
     @Autowired
-    @Lazy
+    // @Lazy
     private RestTemplate template;
+
+    private String response="";
+
 
     // @Value("${microservice.payment-service.endpoints.endpoint.uri}")
     // private String ENDPOINT_URL;
 
     public TransactionResponse saveOrder(TransactionRequest request) {
-        String response="";
         Order order = request.getOrder();
         Payment payment = request.getPayment();
         payment.setOrderId(order.getId());
         payment.setAmount(order.getPrice());
         //rest call
-        Payment paymentResponse = template.postForObject("http://payment-service/payment/doPayment", payment, Payment.class);
-        
+        Payment paymentResponse = template.postForObject("http://localhost:8989/payment/doPayment", payment, Payment.class);
+
         response = paymentResponse.getPaymentStatus().equals("success")?"payment proccesing succesfull and order placed":"there is a failure in payment api, order added to cart";
 
         repository.save(order);
